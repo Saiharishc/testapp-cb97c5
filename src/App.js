8c6a39d679b88f08e93cbbaa3cb21977223ba352
Saiharishc <51015572+Saiharishc@users.noreply.github.com>
2026-07-06 06:21:16 -0500

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TestApp</h1>
+        <p>Welcome to your FastAPI and React app!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
